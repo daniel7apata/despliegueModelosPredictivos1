6db09dd0ce0fda66cc6e231b906d91ae63c8efd6
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2024-11-18 02:35:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ from openai import OpenAI
 import joblib
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
