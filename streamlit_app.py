fb03d8f10efaa3cddf1a91dc1b96225aaf523280
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2024-11-18 02:40:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error,r2_score, me
 best_model_tree = joblib.load('best_model_tree.pkl')
 best_model_rf = joblib.load('best_model_rf.pkl')
 #Cargar conjunto de datos test
-df_test = pd.read_pickle('/content/df_final_tablon_completo_test_encoded.pickle')
+df_test = pd.read_pickle('df_final_tablon_completo_test_encoded.pickle')
 
 
 
