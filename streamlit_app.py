2bb9439882db7d2b80bfa78a989243e341d74957
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2024-11-22 00:56:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,151 +27,223 @@ uploaded_file = st.file_uploader("Sube un archivo CSV o XLSX", type=["csv", "xls
 
 if uploaded_file is not None:
     try:
-        # Verificar el tipo de archivo y leerlo apropiadamente
-        if uploaded_file.name.endswith(".csv"):
-            # Leer archivo CSV
-            df_data = pd.read_csv(uploaded_file, encoding="ISO-8859-1")
-            st.write("Vista previa del archivo CSV:")
-        elif uploaded_file.name.endswith(".xlsx"):
-            # Leer archivo XLSX
-            df_data = pd.read_excel(uploaded_file, engine="openpyxl")
-            st.write("Vista previa del archivo XLSX:")
-        
-        # Mostrar el DataFrame
-        st.write("Datos originales")
-        st.dataframe(df_data)
+        if uploaded_file.name.endswith(".pickle"):
+            df_tablon_completo_test = pd.read_pickle("df_final_tablon_completo_test_encoded.pickle")
+            
+            st.write("Datos transformados")
+            st.dataframe(df_tablon_completo_test)
+            
+            
+            #Traer modelos ya entrenados
+            tree_model = joblib.load('tree_model.pkl')
+            rf_model = joblib.load('rf_model.pkl')
+            #LogisticRegression = joblib.load('LogisticRegression.pkl')
+            
+            best_model_tree = joblib.load('best_model_tree.pkl')
+            best_model_rf = joblib.load('best_model_rf.pkl')        
+            
+            #Separar features de target
+            las_features = df_tablon_completo_test.drop(columns='target_total_mes_accidentes')
+            la_target = df_tablon_completo_test['target_total_mes_accidentes']
+            
+            
+            st.write("Las columnas features")
+            st.dataframe(las_features)
+            
+            st.write("La columna target")
+            st.dataframe(la_target)
+            
+            
+            #Realizar prediccion
+            tree_prediction = tree_model.predict(las_features)
+            rf_prediction = rf_model.predict(las_features)
+            #LogisticRegression_prediction = LogisticRegression.predict(las_features)
+            
+            best_tree_prediction = best_model_tree.predict(las_features)
+            best_rf_prediction = best_model_rf.predict(las_features)
+            
+            
+            
+            #Mostrar predicción
+            
+            df_comparar = pd.DataFrame()
+    
+            df_comparar['target_original'] = la_target
+            #df_comparar['LogisticRegression'] = LogisticRegression_prediction
+            df_comparar['DecisionTreeRegressor'] = tree_prediction
+            df_comparar['Optimizado_DecisionTreeRegressor'] = best_tree_prediction
+            df_comparar['RandomForestRegressor'] = rf_prediction
+            df_comparar['Optimizado_RandomForestRegressor'] = best_rf_prediction
+    
+            
+            
+            st.write("Comparación de predicciones")
+            st.dataframe(df_comparar)
+    
+    
+    
+    
+    
+            st.write("Métricas DecisionTreeRegressor")
+            mae = mean_absolute_error(la_target, tree_prediction)
+            mse = mean_squared_error(la_target, tree_prediction)
+            mape = mean_absolute_percentage_error(la_target, tree_prediction)
+            rmse = mean_squared_error(la_target, tree_prediction, squared=False)
+            r2 = r2_score(la_target, tree_prediction)
+            
+            st.write(f"MAE: {mae}")
+            st.write(f"MSE: {mse}")
+            st.write(f"MAPE: {mape * 100:.2f}%")
+            st.write(f"RMSE: {rmse}")
+            st.write(f"R²: {r2}")
 
-            #cambiar nombres de las columnas
-        cambiar_nombres = {
-          'AÑO': 'año',
-          'MES': 'mes',
-          'CONCESION': 'concesion',
-          'ENTIDAD PRESTADORA': 'entidad_prestadora',
-          'INGRESOS TOTAL': 'ingreso_total',
-          'TRAFICO TOTAL': 'trafico_total',
-          'TOTAL RECLAMOS RESUELTOS': 'total_reclamos_resueltos',
-          'TOTAL RECLAMOS PRESENTADOS': 'total_reclamos_presentados',
-          'TOTAL RECLAMOS EN PROCESO': 'total_reclamos_enproceso',
-          'TOTAL LLAMADAS EMERGENCIAS': 'total_llamadas_emerg',
-          'TOTAL ASISTENCIA MECANICA': 'total_asistencia_mecanica',
-          'TOTAL MES ACCIDENTES': 'target_total_mes_accidentes',
-        }
-        df_data = df_data.rename (columns=cambiar_nombres)
+            
         
-        
-        #Traer los encoders
-        
-        # Cargar el objeto `encoderMMS` desde el archivo
-        encoderMMS = joblib.load('encoderMMS.pkl')
-        
-        # Cargar el objeto `oneHE` desde el archivo
-        oneHE = joblib.load('oneHE.pkl')
-        
-        
-        
-        
-        #aislar features año y mes (*1)
-        aniomes = df_data[['año', 'mes']]
-        
-        
-        
-        #Aplicar el encoder y aislar las features OneHotEncoder (2*)
-        columnas = ['concesion','entidad_prestadora']
-        features_one_hot = pd.DataFrame(oneHE.transform(df_data[columnas]),
-                                      columns=oneHE.get_feature_names_out(columnas),
-                                      index=df_data.index)
-        
-        
-        
-        #Aplica el encoder y aislar las features MinMaxScaler (3*)
-        columnas = ['ingreso_total',	'trafico_total',	'total_reclamos_resueltos',	'total_reclamos_presentados',
-                    'total_reclamos_enproceso','total_llamadas_emerg',	'total_asistencia_mecanica']
-        
-        features_min_max = pd.DataFrame(encoderMMS.transform(df_data[columnas]),
-                                     columns=columnas,
-                                     index=df_data.index)
-        
-        
-        #aislar target
-        columna_target = df_data['target_total_mes_accidentes']
-        
-        
-        # 4 cosas:
-        # año y mes tal cual los datos originales (*1)
-        # transformacion de One Hot encoder (2*)
-        # transformacion de Min Max Scaler (3*)
-        # columna de target bajo el nombre "target_total_mes_accidentes" (*4)
-        df_tablon_completo_test = pd.concat([aniomes   , features_one_hot,   features_min_max,  columna_target],axis=1)
-        
-        st.write("Datos transformados")
-        st.dataframe(df_tablon_completo_test)
-        
-        
-        #Traer modelos ya entrenados
-        tree_model = joblib.load('tree_model.pkl')
-        rf_model = joblib.load('rf_model.pkl')
-        #LogisticRegression = joblib.load('LogisticRegression.pkl')
-        
-        best_model_tree = joblib.load('best_model_tree.pkl')
-        best_model_rf = joblib.load('best_model_rf.pkl')
-        
-        
-        
-        
-        #Separar features de target
-        las_features = df_tablon_completo_test.drop(columns='target_total_mes_accidentes')
-        la_target = df_tablon_completo_test['target_total_mes_accidentes']
-        
-        
-        st.write("Las columnas features")
-        st.dataframe(las_features)
-        
-        st.write("La columna target")
-        st.dataframe(la_target)
-        
-        
-        #Realizar prediccion
-        tree_prediction = tree_model.predict(las_features)
-        rf_prediction = rf_model.predict(las_features)
-        #LogisticRegression_prediction = LogisticRegression.predict(las_features)
-        
-        best_tree_prediction = best_model_tree.predict(las_features)
-        best_rf_prediction = best_model_rf.predict(las_features)
-        
-        
-        
-        #Mostrar predicción
-        
-        df_comparar = pd.DataFrame()
-
-        df_comparar['target_original'] = la_target
-        #df_comparar['LogisticRegression'] = LogisticRegression_prediction
-        df_comparar['DecisionTreeRegressor'] = tree_prediction
-        df_comparar['Optimizado_DecisionTreeRegressor'] = best_tree_prediction
-        df_comparar['RandomForestRegressor'] = rf_prediction
-        df_comparar['Optimizado_RandomForestRegressor'] = best_rf_prediction
-
-        
-        
-        st.write("Comparación de predicciones")
-        st.dataframe(df_comparar)
-
-
-
-
-
-        st.write("Métricas DecisionTreeRegressor")
-        mae = mean_absolute_error(la_target, tree_prediction)
-        mse = mean_squared_error(la_target, tree_prediction)
-        mape = mean_absolute_percentage_error(la_target, tree_prediction)
-        rmse = mean_squared_error(la_target, tree_prediction, squared=False)
-        r2 = r2_score(la_target, tree_prediction)
-        
-        st.write(f"MAE: {mae}")
-        st.write(f"MSE: {mse}")
-        st.write(f"MAPE: {mape * 100:.2f}%")
-        st.write(f"RMSE: {rmse}")
-        st.write(f"R²: {r2}")
+        else:
+            if uploaded_file.name.endswith(".csv"):
+                # Leer archivo CSV
+                df_data = pd.read_csv(uploaded_file, encoding="ISO-8859-1")
+                st.write("Vista previa del archivo CSV:")
+            elif uploaded_file.name.endswith(".xlsx"):
+                # Leer archivo XLSX
+                df_data = pd.read_excel(uploaded_file, engine="openpyxl")
+                st.write("Vista previa del archivo XLSX:")
+            
+            # Mostrar el DataFrame
+            st.write("Datos originales")
+            st.dataframe(df_data)
+    
+                #cambiar nombres de las columnas
+            cambiar_nombres = {
+              'AÑO': 'año',
+              'MES': 'mes',
+              'CONCESION': 'concesion',
+              'ENTIDAD PRESTADORA': 'entidad_prestadora',
+              'INGRESOS TOTAL': 'ingreso_total',
+              'TRAFICO TOTAL': 'trafico_total',
+              'TOTAL RECLAMOS RESUELTOS': 'total_reclamos_resueltos',
+              'TOTAL RECLAMOS PRESENTADOS': 'total_reclamos_presentados',
+              'TOTAL RECLAMOS EN PROCESO': 'total_reclamos_enproceso',
+              'TOTAL LLAMADAS EMERGENCIAS': 'total_llamadas_emerg',
+              'TOTAL ASISTENCIA MECANICA': 'total_asistencia_mecanica',
+              'TOTAL MES ACCIDENTES': 'target_total_mes_accidentes',
+            }
+            df_data = df_data.rename (columns=cambiar_nombres)
+            
+            
+            #Traer los encoders
+            
+            # Cargar el objeto `encoderMMS` desde el archivo
+            encoderMMS = joblib.load('encoderMMS.pkl')
+            
+            # Cargar el objeto `oneHE` desde el archivo
+            oneHE = joblib.load('oneHE.pkl')
+            
+            
+            
+            
+            #aislar features año y mes (*1)
+            aniomes = df_data[['año', 'mes']]
+            
+            
+            
+            #Aplicar el encoder y aislar las features OneHotEncoder (2*)
+            columnas = ['concesion','entidad_prestadora']
+            features_one_hot = pd.DataFrame(oneHE.transform(df_data[columnas]),
+                                          columns=oneHE.get_feature_names_out(columnas),
+                                          index=df_data.index)
+            
+            
+            
+            #Aplica el encoder y aislar las features MinMaxScaler (3*)
+            columnas = ['ingreso_total',	'trafico_total',	'total_reclamos_resueltos',	'total_reclamos_presentados',
+                        'total_reclamos_enproceso','total_llamadas_emerg',	'total_asistencia_mecanica']
+            
+            features_min_max = pd.DataFrame(encoderMMS.transform(df_data[columnas]),
+                                         columns=columnas,
+                                         index=df_data.index)
+            
+            
+            #aislar target
+            columna_target = df_data['target_total_mes_accidentes']
+            
+            
+            # 4 cosas:
+            # año y mes tal cual los datos originales (*1)
+            # transformacion de One Hot encoder (2*)
+            # transformacion de Min Max Scaler (3*)
+            # columna de target bajo el nombre "target_total_mes_accidentes" (*4)
+            df_tablon_completo_test = pd.concat([aniomes   , features_one_hot,   features_min_max,  columna_target],axis=1)
+            
+            st.write("Datos transformados")
+            st.dataframe(df_tablon_completo_test)
+            
+            
+            #Traer modelos ya entrenados
+            tree_model = joblib.load('tree_model.pkl')
+            rf_model = joblib.load('rf_model.pkl')
+            #LogisticRegression = joblib.load('LogisticRegression.pkl')
+            
+            best_model_tree = joblib.load('best_model_tree.pkl')
+            best_model_rf = joblib.load('best_model_rf.pkl')
+            
+            
+            
+            
+            #Separar features de target
+            las_features = df_tablon_completo_test.drop(columns='target_total_mes_accidentes')
+            la_target = df_tablon_completo_test['target_total_mes_accidentes']
+            
+            
+            st.write("Las columnas features")
+            st.dataframe(las_features)
+            
+            st.write("La columna target")
+            st.dataframe(la_target)
+            
+            
+            #Realizar prediccion
+            tree_prediction = tree_model.predict(las_features)
+            rf_prediction = rf_model.predict(las_features)
+            #LogisticRegression_prediction = LogisticRegression.predict(las_features)
+            
+            best_tree_prediction = best_model_tree.predict(las_features)
+            best_rf_prediction = best_model_rf.predict(las_features)
+            
+            
+            
+            #Mostrar predicción
+            
+            df_comparar = pd.DataFrame()
+    
+            df_comparar['target_original'] = la_target
+            #df_comparar['LogisticRegression'] = LogisticRegression_prediction
+            df_comparar['DecisionTreeRegressor'] = tree_prediction
+            df_comparar['Optimizado_DecisionTreeRegressor'] = best_tree_prediction
+            df_comparar['RandomForestRegressor'] = rf_prediction
+            df_comparar['Optimizado_RandomForestRegressor'] = best_rf_prediction
+    
+            
+            
+            st.write("Comparación de predicciones")
+            st.dataframe(df_comparar)
+    
+    
+    
+    
+    
+            st.write("Métricas DecisionTreeRegressor")
+            mae = mean_absolute_error(la_target, tree_prediction)
+            mse = mean_squared_error(la_target, tree_prediction)
+            mape = mean_absolute_percentage_error(la_target, tree_prediction)
+            rmse = mean_squared_error(la_target, tree_prediction, squared=False)
+            r2 = r2_score(la_target, tree_prediction)
+            
+            st.write(f"MAE: {mae}")
+            st.write(f"MSE: {mse}")
+            st.write(f"MAPE: {mape * 100:.2f}%")
+            st.write(f"RMSE: {rmse}")
+            st.write(f"R²: {r2}")
 
 
     except Exception as e:
